3663d8a3ac7fb1801963f3c08343c31e907b46c5
Farrukh Nauman <farrukhnauman@yahoo.com>
2023-04-03 17:50:58 +0200

    added dummy data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,81 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import plost
+import plotly.express as px
 
-"""
-# Welcome to Streamlit!
+## Load data
+df_schools = pd.read_csv('./data/schools_anon.csv')
+df_students = pd.read_csv('./data/students_risk_anon.csv')
+df_risk_per_school = df_students.groupby('School').agg({'Risk_factor': 'mean'}).reset_index()
+# print(df_risk_per_school.head())
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+## Page setup
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+## Set page title
+st.markdown(
+    """
+    # School dashboard
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    """,
+    unsafe_allow_html=True
+)
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    
+# st.sidebar.header('School dashboard')
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+st.sidebar.subheader('Choose school property')
+chosen_property = st.sidebar.selectbox('Select data', ('students per teacher', 'teachers with degree', 'teachers with permanent job'))
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+st.sidebar.subheader('School selection')
+chosen_school = st.sidebar.selectbox('Select school', df_schools.school.unique(), index=0)
+# plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# Row A
+st.markdown('### Overall commune metrics')
+col1, col2, col3 = st.columns(3)
+col1.metric("Eligibility", "77%", "2%")
+col2.metric("Attendance", "83%", "-4%")
+col3.metric("Math", "82%", "-3%")
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Row B
+c1, c2 = st.columns((7, 3))
+with c1:
+    st.markdown('### Students at risk in each school')
+    st.plotly_chart(
+        px.bar(
+            df_risk_per_school, 
+            x='School',
+            y='Risk_factor', 
+            color='Risk_factor',
+            title='Risk Factor per School', 
+        ), 
+        use_container_width=True
+    )
+with c2:
+    st.markdown('### School characteristics')
+    
+    st.plotly_chart(
+        px.bar(
+            df_schools[df_schools.school == chosen_school], 
+            x='year',
+            y=chosen_property, 
+            color=chosen_property,
+        )
+    )
+
+# Row C
+st.markdown('### Eligibility over time')
+# st.line_chart(df_schools, x='year', y='eligibility', height=plot_height)
+st.plotly_chart(
+    px.line(
+        df_schools[df_schools.school == chosen_school],
+        x='year',
+        y='eligibility', 
+    ), 
+    use_container_width=True
+)
